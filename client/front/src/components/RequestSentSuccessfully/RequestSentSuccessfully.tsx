77634aed6f1bbe259b193eca25388312dc3a5ce4
Diana <isakovdiana1@gmail.com>
2021-02-13 18:19:10 +0200

    thank you component

--- client/front/src/components/RequestSentSuccessfully/RequestSentSuccessfully.tsx
+++ client/front/src/components/RequestSentSuccessfully/RequestSentSuccessfully.tsx
@@ -1,28 +1,25 @@
 import React from "react";
 import { MDBJumbotron, MDBContainer, MDBRow, MDBCol } from "mdbreact";
-import './RequestSentSuccessfully.css';
+import "./RequestSentSuccessfully.css";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
-
-const RequestSentSuccessfully = () => {
-  return ( 
-    
-    <MDBContainer className="mt-5 text-center">
-      <MDBRow>
-        <MDBCol>
-          <MDBJumbotron className="Submited">
-            <h2 className="h1 display-3">Thank You</h2>
-            <p className="lead">
-              Your application was successfully sent.
-            </p>
-            <hr className="my-2" />
-            <p>
-              We'll contact you soon.
-            </p>
-          </MDBJumbotron>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-
+const RequestSentSuccessfully: React.FC = () => {
+  return (
+    <div>
+      <MDBContainer className="mt-5 text-center">
+        <MDBRow>
+          <MDBCol>
+            <MDBJumbotron className="Submited">
+              <h2 className="h1 display-3">Thank You</h2>
+              <p className="lead">Your application was successfully sent.</p>
+              <hr className="my-2" />
+              <p>We'll contact you soon.</p>
+            </MDBJumbotron>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 };
 
