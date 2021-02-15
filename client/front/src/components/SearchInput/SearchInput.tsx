cd79b128d49bd249f503f9cd2b4b3c51ac1fffcd
Aviv Miranda <Aviv943@gmail.com>
2021-02-15 18:19:20 +0200

    fixed what matan couldn't again

--- client/front/src/components/SearchInput/SearchInput.tsx
+++ client/front/src/components/SearchInput/SearchInput.tsx
@@ -20,25 +20,23 @@ export default function SearchInput(props: ISearchProps) {
   }, [debouncedSearchQuery, onChangeSearchQuery]);
 
   return (
-    <div className="main-container">
-      <MDBCard>
-        <div className="header pt-3 grey lighten-2">
-          <MDBRow className="d-flex justify-content-start">
-            <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5" >Search:</h3>
-          </MDBRow>
-        </div>
-        {/* <h3 htmlFor="search" >Search animals here!</h3> */}
-        <MDBCardBody>
-          <input
-            id="search"
-            className="form-control full-width"
-            type="search"
-            placeholder="Search..."
-            aria-label="Search"
-            onChange={(event) => setSearchQuery(event.target.value)}
-          />
-        </MDBCardBody>
-      </MDBCard>
-    </div>
+    <MDBCard>
+      <div className="header pt-3 grey lighten-2">
+        <MDBRow className="d-flex justify-content-start">
+          <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5" >Search:</h3>
+        </MDBRow>
+      </div>
+      {/* <h3 htmlFor="search" >Search animals here!</h3> */}
+      <MDBCardBody>
+        <input
+          id="search"
+          className="form-control full-width"
+          type="search"
+          placeholder="Search..."
+          aria-label="Search"
+          onChange={(event) => setSearchQuery(event.target.value)}
+        />
+      </MDBCardBody>
+    </MDBCard>
   );
 }
