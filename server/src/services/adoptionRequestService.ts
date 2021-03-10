2676bdae9cfd7a83c3428621b962380985709802
yonatangross <yonatan2gross@gmail.com>
2021-03-10 20:26:56 +0200

    updated getAll according to backend

--- server/src/services/adoptionRequestService.ts
+++ server/src/services/adoptionRequestService.ts
@@ -1,65 +1,50 @@
 import { IPet } from '../interfaces/IPet';
-import { Service } from 'typedi'
+import { Service } from 'typedi';
 import { IAdoptionRequest } from '../interfaces/IAdoptionRequest';
 import AdoptionRequest from '../models/adoptionRequest';
 
 @Service()
 export default class AdoptionRequestService {
-    public async getById(id: string) {
-        const adoptionRequest: IAdoptionRequest | null = await AdoptionRequest.findById(id).populate('pet');
-        return adoptionRequest;
-    }
+  public async getById(id: string) {
+    const adoptionRequest: IAdoptionRequest | null = await AdoptionRequest.findById(id).populate('pet');
+    return adoptionRequest;
+  }
 
-    public async getAll() {
-        const adoptionRequests: IAdoptionRequest[] = await AdoptionRequest.find().populate('pet');
-        return adoptionRequests;
-    }
+  public async getAll() {
+    const adoptionRequests: IAdoptionRequest[] = await AdoptionRequest.find();
+    return adoptionRequests;
+  }
 
-    public async create(req: any, pet: IPet) {
-        const body = req as Pick<
-            IAdoptionRequest,
-            'fullName' | 'email' | 'phoneNumber' | 'address' | 'message'
-        >;
+  public async create(req: any, pet: IPet) {
+    const body = req as Pick<IAdoptionRequest, 'fullName' | 'email' | 'phoneNumber' | 'address' | 'message'>;
 
-        const adoptionRequest = new AdoptionRequest({
-            pet: pet,
-            fullName: body.fullName,
-            email: body.email,
-            phoneNumber: body.phoneNumber,
-            address: body.address,
-            message: body.message,
-        });
+    const adoptionRequest = new AdoptionRequest({
+      pet: pet,
+      fullName: body.fullName,
+      email: body.email,
+      phoneNumber: body.phoneNumber,
+      address: body.address,
+      message: body.message,
+    });
 
-        const createdAdoptionRequest: IAdoptionRequest = await adoptionRequest.save();
+    const createdAdoptionRequest: IAdoptionRequest = await adoptionRequest.save();
 
-        return { adoptionRequest: createdAdoptionRequest };
-    }
+    return { adoptionRequest: createdAdoptionRequest };
+  }
 
+  public async updateById(adoptionRequestId: string, req: any) {
+    const adoptionRequest: IAdoptionRequest | null = await AdoptionRequest.findByIdAndUpdate({ _id: adoptionRequestId }, req.body);
+    return {
+      message: 'Adoption request updated',
+      adoptionRequest: adoptionRequest,
+    };
+  }
 
-    public async updateById(adoptionRequestId: string, req: any) {
-        const adoptionRequest: IAdoptionRequest | null = await AdoptionRequest.findByIdAndUpdate(
-            { _id: adoptionRequestId },
-            req.body
-        );
-        return {
-            message: 'Adoption request updated',
-            adoptionRequest: adoptionRequest,
-        };
-    }
-
-    public async deleteById(adoptionRequestId: string) {
-        const deletedAdoptionRequest: IAdoptionRequest | null = await AdoptionRequest.findByIdAndRemove(adoptionRequestId);
-        return {
-            message: `Adoption request of ${adoptionRequestId} deleted`,
-            adoptionRequest: deletedAdoptionRequest,
-        };
-    }
+  public async deleteById(adoptionRequestId: string) {
+    const deletedAdoptionRequest: IAdoptionRequest | null = await AdoptionRequest.findByIdAndRemove(adoptionRequestId);
+    return {
+      message: `Adoption request of ${adoptionRequestId} deleted`,
+      adoptionRequest: deletedAdoptionRequest,
+    };
+  }
 }
-
-
-
-
-
-
-
-
